e2260fc68ef2932d1b1581712f3afef7d3fd0395
MarcelaZenteno93 <126695026+MarcelaZenteno93@users.noreply.github.com>
2023-11-06 20:20:30 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String,DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -10,47 +10,42 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     user_name = Column(String(100), nullable=False)
     password = Column(String(100), nullable=False)
-    email= Column(String(250) ,nullable=False)
+    email = Column(String(250), nullable=False)
 
-    favorites = relationship('Favorite', back_populates='user')
+    favorite = relationship('Favorite', back_populates='user')
 
 class Planet(Base):
     __tablename__ = 'planet'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     climate = Column(String(100))
     terrain = Column(String(100))
 
-    favorites = relationship('Favorite', back_populates='planet')
+    favorite = relationship('Favorite', back_populates='planet')
 
 class Character(Base):
     __tablename__ = 'character'
-    id = Column(Integer,primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     birth_year = Column(String(20))
     gender = Column(String(20))
 
-    favorites = relationship('Favorite', back_populates='character')
+    favorite = relationship('Favorite', back_populates='character')
 
 class Favorite(Base):
     __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
-    user_id=Column(Integer, ForeignKey('user.id'),nullable=False)
-    planet_id=Column(Integer, ForeignKey('planet.id'))
-    character_id=Column(Integer, ForeignKey('character_id'))
-    timestamp= Column(DateTime(timezone=True),server_default=func.now())
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
 
-    user = relationship(User, back_populates='favorites')
-    planet = relationship(Planet, back_populates='favorites')
-    character = relationship(Character,  back_populates='favorites')
-
+    user = relationship('User', back_populates='favorite')
+    planet = relationship('Planet', back_populates='favorite')
+    character = relationship('Character', back_populates='favorite')
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
